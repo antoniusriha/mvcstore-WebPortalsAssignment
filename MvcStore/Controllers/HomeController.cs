a5b850392554149cb8ca7ab41f771b014a38aba0
Antonius Riha <antoniusriha@gmail.com>
2012-11-03 19:04:03 +0100

    init

---
+++ MvcStore/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using MvcStore.Models;
+
+namespace MvcStore.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index ()
+		{
+			var homeVm = new HomeViewModel () { WelcomeMessage = "Welcome to ASP.NET MVC on Mono!" };
+			return View (homeVm);
+		}
+		
+		public ActionResult Category ()
+		{
+			var cat = new Category () { Name = "Cat" };
+			cat.Products.Add (new Product () { Name = "P1" });
+			cat.Products.Add (new Product () { Name = "P2" });
+			
+			return View (cat);
+		}
+	}
+}
---
+++ MvcStore/Global.asax.cs
@@ -0,0 +1,37 @@
+
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace MvcStore
+{
+	public class MvcApplication : HttpApplication
+	{
+		public static void RegisterRoutes (RouteCollection routes)
+		{
+			routes.IgnoreRoute ("{resource}.axd/{*pathInfo}");
+
+			routes.MapRoute (
+                "Default",
+                "{controller}/{action}/{id}",
+                new { controller = "Home", action = "Index", id = "" }
+			);
+			routes.MapRoute ("Category", "Category", new {
+				controller = "Home", action = "Category", id = "" }
+			);
+		}
+
+		protected void Application_Start ()
+		{
+			AreaRegistration.RegisterAllAreas ();
+			RegisterRoutes (RouteTable.Routes);
+			
+			// Setup database
+			System.IO.File.Create ("tsts.txt");
+		}
+	}
+}
